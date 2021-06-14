e46bb7fd31d685c8edac5c91734b8c865b98462f
Kelly Selden <kellyselden@gmail.com>
2021-06-13 18:55:27 -0700

    allow dynamicImport

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ module.exports = {
   rules: {
     'strict': 'error',
 
+    'node/no-unsupported-features/es-syntax': ['error', {
+      'ignores': [
+        // remove once https://github.com/mysticatea/eslint-plugin-node/issues/250 is fixed
+        'dynamicImport'
+      ]
+    }],
+
     'json-files/restrict-ranges': [2, { pinUnstable: true }]
   }
 };
