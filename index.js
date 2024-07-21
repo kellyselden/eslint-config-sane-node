52fe1eb691b66ed391659afd136accb86bc12cac
Kelly Selden <602423+kellyselden@users.noreply.github.com>
2024-07-21 16:07:20 -0700

    fix https://github.com/eslint-community/eslint-plugin-n/pull/13

--- index.js
+++ index.js
@@ -12,13 +12,6 @@ module.exports = {
   rules: {
     'strict': 'error',
 
-    'n/no-unsupported-features/es-syntax': ['error', {
-      'ignores': [
-        // remove once https://github.com/mysticatea/eslint-plugin-node/issues/250 is fixed
-        'dynamicImport'
-      ]
-    }],
-
     'json-files/restrict-ranges': [2, { pinUnstable: true }]
   }
 };
