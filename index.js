a0c64709d36df7cfdd8aa01e815ff7e4a06b96c8
Kelly Selden <602423+kellyselden@users.noreply.github.com>
2025-08-24 17:13:03 -0400

    ESLint v9

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 'use strict';
 
-module.exports = {
-  env: {
-    node: true
-  },
-  extends: [
-    'sane',
-    'plugin:n/recommended'
-  ],
-  plugins: [
-    'json-files'
-  ],
-  rules: {
-    'strict': 'error',
+const {
+  defineConfig
+} = require('@eslint/config-helpers');
 
-    'json-files/restrict-ranges': [2, { pinUnstable: true }]
+const globals = require('globals');
+const jsonFiles = require('eslint-plugin-json-files');
+const node = require('eslint-plugin-n');
+const sane = require('eslint-config-sane');
+
+module.exports = defineConfig([
+  node.configs['flat/recommended-script'],
+  sane,
+  {
+    files: [
+      '**/*.js',
+      '**/*.cjs',
+      '**/*.mjs'
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.node
+      }
+    },
+    plugins: {
+      'json-files': jsonFiles
+    },
+    rules: {
+      'strict': 'error',
+
+      'json-files/restrict-ranges': [2, {
+        pinUnstable: true
+      }]
+    }
   }
-};
+]);
